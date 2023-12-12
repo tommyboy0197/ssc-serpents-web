5956f93c4efc710ba98e88492ef5badb1c0a1898
tommyboy0197 <153368368+tommyboy0197@users.noreply.github.com>
2023-12-12 09:33:26 +0000

    1
    1

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function About() {
     document.getElementById("root").style.height = "100vh"
     return (
-        <>
-        </>
+        <div style={{margin: "auto",textAlign: 'center'}}>
+            <h1 style={{fontSize: '25px'}}>We are the ssc serpents</h1>
+        </div>
     )
 };
