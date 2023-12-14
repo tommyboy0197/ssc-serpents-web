e228b15982f64e314c34d7fe5705e5f00415bc7f
tommyboy0197 <153368368+tommyboy0197@users.noreply.github.com>
2023-12-14 13:39:56 +0000

    1
    1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,14 +6,12 @@ export default function Home() {
     return (
         <div style={{margin: 'auto', textAlign: 'center'}}>
             <h1 style={{marginTop: "2.5vh",
-                marginLeft: "42.5vh",
                 fontSize: "75px",
                 fontWeight: "bold",color: '#1C6E8C'}}>Welcome To The</h1>
             <h1 style={{marginTop: "2.5vh",
-                marginLeft: "30vh",
                 fontSize: "125px",
                 fontWeight: "bold",color: '#1C6E8C'}}>SSC SERPENTS</h1>
-            <Button as="a" href="/about" sx={{marginLeft: "70vh",marginTop: '15px',backgroundColor: '#0d77db',textColor: 'black'}}>About us</Button>
+            <Button as="a" href="/about" sx={{marginTop: '15px',backgroundColor: '#0d77db',textColor: 'black'}}>About us</Button>
         </div>
     )
 }
